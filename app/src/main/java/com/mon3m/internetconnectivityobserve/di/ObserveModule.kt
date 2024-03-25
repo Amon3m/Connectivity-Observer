b0162a2aad0512ff93f9c9dfc60126fc28abe17d
Ahmed Abd El-Monem <112562093+Amon3m@users.noreply.github.com>
2024-03-25 19:45:11 +0200

    internet connectivity observe

--- app/src/main/java/com/mon3m/internetconnectivityobserve/di/ObserveModule.kt
+++ app/src/main/java/com/mon3m/internetconnectivityobserve/di/ObserveModule.kt
@@ -6,13 +6,14 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class ObserveModule {
     @Binds
-    @Singleton
+    @ViewModelScoped
     abstract fun bindNetworkConnectivityObserver(networkConnectivityObserver: NetworkConnectivityObserver): ConnectivityObserver
 
 }
