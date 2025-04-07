6bbccd75c7663859a2e85843563294a58336eb09
Ahmed Abd El-Monem <112562093+Amon3m@users.noreply.github.com>
2025-04-07 14:06:57 +0200

    Update project configuration and enhance network status handling

--- app/src/main/java/com/mon3m/internetconnectivityobserve/present/viewmodel/NetworkStatusViewModel.kt
+++ app/src/main/java/com/mon3m/internetconnectivityobserve/present/viewmodel/NetworkStatusViewModel.kt
@@ -5,25 +5,43 @@ import androidx.lifecycle.viewModelScope
 import com.mon3m.internetconnectivityobserve.data.netwrok.ConnectivityObserver
 import com.mon3m.internetconnectivityobserve.domain.usecase.ObserveNetworkStatusUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NetworkStatusViewModel @Inject constructor(
-    private val observeNetworkStatusUseCase: ObserveNetworkStatusUseCase
-
-): ViewModel(){
-    private val _networkStatus = MutableStateFlow<ConnectivityObserver.NetworkStatus>(
-        ConnectivityObserver.NetworkStatus.Unavailable)
-    val networkStatus: StateFlow<ConnectivityObserver.NetworkStatus> get() = _networkStatus
+    private val connectivityObserver: ConnectivityObserver
+) : ViewModel() {
+    private val _networkStatus = MutableSharedFlow<ConnectivityObserver.NetworkStatus>(
+        replay = 0,
+        extraBufferCapacity = 1
+    )
+    val networkStatus: SharedFlow<ConnectivityObserver.NetworkStatus> = _networkStatus
 
     init {
         viewModelScope.launch {
-            observeNetworkStatusUseCase.execute().collect { status ->
-                _networkStatus.value = status
-            }
+            var isFirstEmission = true
+
+            connectivityObserver.observe()
+                .transform { status ->
+                    if (isFirstEmission) {
+                        isFirstEmission = false
+                        // Skip the first emission if it's Available
+                        if (status == ConnectivityObserver.NetworkStatus.Available) return@transform
+                    }
+                    emit(status)
+                }
+                .debounce(3000)
+                .collectLatest { status ->
+                    _networkStatus.emit(status)
+                }
         }
     }
 }
